5b6e7e38ec87d1cd0f5b262c420da1a99859d576
colorodriguezf <colitorodriguez97@gmail.com>
2022-02-08 14:40:51 -0300

    Listo los 4 calculos posibles, solo falta css

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 "use strict"
 
 document.addEventListener("DOMContentLoaded",calcular);
-let resultado= document.getElementById("resultado");
+document.getElementById("igual").addEventListener("click", resultadoFinal);
+document.getElementById("reiniciar").addEventListener("click", reiniciar);
+let calculo= document.getElementById("calculo");
 let total= [];
+let resultadoTotal =document.getElementById("resultado");
 
 function calcular() {
 let valor = document.querySelectorAll(".valor");
@@ -13,5 +16,17 @@ for (let v of valor) {
 
 function obtenerValor() {
 total+=  event.currentTarget.value;
+calculo.value=total;
 console.log(total);
+}
+
+function resultadoFinal() {
+    resultadoTotal.value= eval(total);
+    total=resultadoTotal.value;
+}
+function reiniciar() {
+    calculo.value ="";
+    total="";
+    resultadoTotal.value=0;
+
 }
