6cd360a246746fc6aa492c2b59c264379fcbf474
colorodriguezf <colitorodriguez97@gmail.com>
2022-02-08 19:24:29 -0300

    Calculadora terminada, se le agrego sonido a los botones, fondo y cambio de colores

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # caluladora
+Calculadora realizada con HTML, CSS y JS.
+Es responsive, y tiene diferentes estilos para los distintos tamaños de pantalla.
--- js/app.js
+++ js/app.js
@@ -9,22 +9,35 @@ let resultadoTotal =document.getElementById("resultado");
 
 function calcular() {
 let valor = document.querySelectorAll(".valor");
+
 for (let v of valor) {
     v.addEventListener("click", obtenerValor);
 }
-}
 
+
+}
 function obtenerValor() {
+const sonido = new Audio();
+sonido.src="sonido/ES_Bubble Blip 2 - SFX Producer.mp3";
+sonido.play();
+
 total+=  event.currentTarget.value;
 calculo.value=total;
 console.log(total);
 }
 
 function resultadoFinal() {
+    const audioResultado = new Audio();
+    audioResultado.src="sonido/ES_Squeak Drip - SFX Producer.mp3";
+    audioResultado.play();
     resultadoTotal.value= eval(total);
     total=resultadoTotal.value;
 }
 function reiniciar() {
+    const audioResultado = new Audio();
+    audioResultado.src="sonido/ES_Squeak Drip - SFX Producer.mp3";
+    audioResultado.play();
+    
     calculo.value ="";
     total="";
     resultadoTotal.value=0;
